04e7c8c0a5ac6fe8d0f995e3c439e31f5a91feaa
abenbyy <andrbenaya@gmail.com>
2022-02-06 14:26:51 +0700

    added favourites

--- components/Card/CardContent.jsx
+++ components/Card/CardContent.jsx
@@ -2,7 +2,8 @@ export default function CardContent(props) {
   const { className, children } = props;
 
   return (
-    <div className={`p-2 w-full h-full flex flex-col ${className}`}>
+    <div
+      className={`p-2 w-full h-full flex flex-row justify-between ${className}`}>
       {children}
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,56 @@
+import { Alert, capitalize, Snackbar } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardMedia } from "../components";
-import { capitalize, zero_pad } from "../utilities";
+import HeartIcon from "../components/Icons/Heart";
+import { zero_pad } from "../utilities";
 
 export default function Home() {
   const [limit, setLimit] = useState(12);
+  const vertical = "top";
+  const horizontal = "right";
   const [maxPage, setMaxPage] = useState(0);
   const [page, setPage] = useState(1);
   const [pokemons, setPokemons] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [snackbarOptions, setSnackbarOptions] = useState({
+    open: false,
+    message: "",
+    severity: "success",
+  });
+
+  const handleSnackbarClose = () => {
+    setSnackbarOptions({
+      open: false,
+    });
+  };
+
+  const handleFavourites = (id, name) => {
+    let newVal = [];
+    let action = "";
+    if (favourites.includes(id)) {
+      let arr = favourites;
+      arr.splice(arr.indexOf(id), 1);
+      newVal = arr;
+      action = "removed from";
+    } else {
+      newVal = [...favourites, id];
+      action = "added to";
+    }
+    setFavourites(newVal);
+    localStorage.setItem("favourites", JSON.stringify(newVal));
+    setSnackbarOptions({
+      open: true,
+      message: `${capitalize(name)} has been ${action} your favourites`,
+    });
+  };
+
+  const fetchFavourites = () => {
+    if (localStorage.getItem("favourites") === null) {
+      localStorage.setItem("favourites", JSON.stringify([]));
+    }
+    const json = JSON.parse(localStorage.getItem("favourites"));
+    if (json.length > 0) setFavourites(json);
+  };
 
   const fetchData = () => {
     fetch(
@@ -25,16 +69,24 @@ export default function Home() {
   };
 
   useEffect(() => {
+    if (favourites.length === 0) fetchFavourites();
     fetchData();
-  }, []);
+  }, [favourites]);
 
   return (
     <div className='min-h-screen min-w-screen bg-gray-100'>
+      <Snackbar
+        open={snackbarOptions.open}
+        anchorOrigin={{ vertical, horizontal }}
+        autoHideDuration={1500}
+        onClose={handleSnackbarClose}>
+        <Alert severity={snackbarOptions.severity}>
+          {snackbarOptions.message}
+        </Alert>
+      </Snackbar>
       <div className='container mx-auto p-5 grid grid-cols-4 gap-4'>
         {pokemons?.map((p, i) => {
           const id = (page - 1) * limit + i + 1;
-          console.log(id);
-          console.log(zero_pad(id));
           return (
             <Card key={i}>
               <CardMedia
@@ -42,11 +94,19 @@ export default function Home() {
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
               />
               <CardContent>
-                <div className='text-md text-gray-400'>
-                  <i>#{zero_pad(id ?? "", 4)}</i>
+                <div>
+                  <div className='text-md text-gray-400'>
+                    <i>#{zero_pad(id ?? "", 4)}</i>
+                  </div>
+                  <div className='mt-2 text-3xl font-handwritten'>
+                    {capitalize(p?.name)}
+                  </div>
                 </div>
-                <div className='mt-2 text-3xl font-handwritten'>
-                  {capitalize(p?.name)}
+                <div>
+                  <HeartIcon
+                    solid={favourites.includes(id)}
+                    customClickEvent={() => handleFavourites(id, p?.name)}
+                  />
                 </div>
               </CardContent>
             </Card>
